a5cf18c519b57c177fecbc5b07b287776c750c1b
GuimelGaNu <guimelgn96@hotmail.com>
2024-01-24 09:59:26 -0600

    Initial Commit

---
+++ holamundo.js
@@ -0,0 +1,12 @@
+console.log("Hola Perrin!");
+
+function cambiarNombre (obj) {
+    obj.nombre = "Pedro"
+}
+
+let tipito = {nombre: 'Jorge', apellido: 'Garcia'}
+
+cambiarNombre(tipito)
+
+console.log(tipito)
+
